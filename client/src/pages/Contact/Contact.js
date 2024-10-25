2c93d2612f408d1f50ab863e36a674a8b41e9668
SAGAR KUMAR DAS <108587278+Sagarkumardas2002@users.noreply.github.com>
2024-10-25 15:42:12 +0530

    Update Contact.js

--- client/src/pages/Contact/Contact.js
+++ client/src/pages/Contact/Contact.js
@@ -20,7 +20,7 @@ const Contact = () => {
 
         try {
             // Send data to backend API
-            const res = await axios.post("http://localhost:5000/api/v1/portfolio/sendEmail", {
+            const res = await axios.post("api/v1/portfolio/sendEmail", {
                 name,
                 email,
                 message: msg,  
