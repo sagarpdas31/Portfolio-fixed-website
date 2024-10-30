fc0739860659f58ca8d41c80412d7515d9a409f8
SAGAR KUMAR DAS <mrsagarkumardas5g9327@gmail.com>
2024-10-30 23:35:36 +0530

    Update cron.js

--- server/cron/cron.js
+++ server/cron/cron.js
@@ -1,5 +1,22 @@
+import cron from "cron";
+import https from "https";
 
-//export default job;
+const URL = "https://portfolio-fixed-website-w69n.onrender.com/api/v1/category/get-category";
+const job = new cron.CronJob("*/14 * * * *", function () {
+    https
+        .get(URL, (res) => {
+            if (res.statusCode === 200) {
+                console.log("GET request sent successfully");
+            } else {
+                console.log("GET request failed", res.statusCode);
+            }
+        })
+        .on("error", (e) => {
+            console.error("Error while sending request", e);
+        });
+});
+
+export default job;
 
 // CRON JOB EXPLANATION:
 // Cron jobs are scheduled tasks that run periodically at fixed intervals or specific times
