a442fb37cf910282da2a777f6e416da9d079f289
SAGAR KUMAR DAS <108587278+Sagarkumardas2002@users.noreply.github.com>
2025-08-20 10:24:40 +0530

    Update Home.js

--- client/src/pages/Homepage/Home.js
+++ client/src/pages/Homepage/Home.js
@@ -13,7 +13,7 @@ const Home = () => {
         setTheme((prevState) => (prevState === "light" ? "dark" : "light"));
     };
     
-    const resumeUrl = 'https://drive.google.com/file/d/1fWpqxJBehKwG-NwtsaehZU9yQjgsCgjd/view?usp=drive_link';
+    const resumeUrl = 'https://drive.google.com/file/d/1wMLik36_zU2gRyFpWSNTgAAbgsE4MNI6/view?usp=drive_link';
     
     return (
         <>
