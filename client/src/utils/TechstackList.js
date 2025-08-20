5f16b3938efc27b890b441f690dbf1b7a334bcb1
SAGAR KUMAR DAS <108587278+Sagarkumardas2002@users.noreply.github.com>
2025-08-20 10:49:50 +0530

    Update TechstackList.js

--- client/src/utils/TechstackList.js
+++ client/src/utils/TechstackList.js
@@ -1,111 +1,113 @@
+correct this 
 import { BsBootstrap, BsFiletypeCss, BsFiletypeHtml } from "react-icons/bs";
 import { CgCPlusPlus } from "react-icons/cg";
 import { DiPython } from "react-icons/di";
 import { FaNodeJs } from "react-icons/fa6";
 
 import {
-  SiJavascript,
-  SiMui,
-  SiReact,
-  SiTailwindcss,
-  SiMongodb,
-  SiMysql,
-  SiGithub,
-  SiFirebase,
-  SiExpress,
-  SiJava,
+    SiJavascript,
+    SiMui,
+    SiReact,
+    SiTailwindcss,
+    SiMongodb,
+    SiMysql,
+    SiGithub,
+    SiFirebase,
+    SiExpress,
+    SiJava,
+    SiTypescript,
+    SiDesign Patterns,
+    SiSystemDesign,
+
 } from "react-icons/si";
 import { TbBrandNextjs } from "react-icons/tb";
 
 export const TechStackList = [
+    {
+        _id: 1,
+        name: "HTML",
+        icon: BsFiletypeHtml,
+    },
+    {
+        _id: 2,
+        name: "CSS",
+        icon: BsFiletypeCss,
+    },
+    {
+        _id: 3,
+        name: "Bootstrap",
+        icon: BsBootstrap,
+    },
+    {
+        _id: 4,
+        name: "Javascript",
+        icon: SiJavascript,
+    },
+
+    {
+        _id: 5,
+        name: "React JS",
+        icon: SiReact,
+    },
+    {
+        _id: 6,
+        name: "Nextjs",
+        icon: TbBrandNextjs,
+    },
+    {
+        _id: 7,
+        name: "React MUI",
+        icon: SiMui,
+    },
+    {
+        _id: 8,
+        name: "Tailwind ",
+        icon: SiTailwindcss,
+    },
+    {
+        _id: 9,
+        name: "Node JS ",
+        icon: FaNodeJs,
+    },
+    {
+        _id: 10,
+        name: "Express JS ",
+        icon: SiExpress,
+    },
+    {
+        _id: 12,
+        name: "Mongodb ",
+        icon: SiMongodb,
+    },
+    {
+        _id: 13,
+        name: "SQL ",
+        icon: SiMysql,
+    },
+    {
+        _id: 14,
+        name: "Firebase ",
+        icon: SiFirebase,
+    },
+    {
+        _id: 15,
+        name: "Git / Github ",
+        icon: SiGithub,
+    },
+    {
+        _id: 16,
+        name: "Python ",
+        icon: DiPython,
+    },
+    {
+        _id: 17,
+        name: "C++ ",
+        icon: CgCPlusPlus,
+    },
   {
-    _id: 1,
-    name: "HTML",
-    icon: BsFiletypeHtml,
-  },
-  {
-    _id: 2,
-    name: "CSS",
-    icon: BsFiletypeCss,
-  },
-  {
-    _id: 3,
-    name: "Bootstrap",
-    icon: BsBootstrap,
-  },
-  {
-    _id: 4,
-    name: "Javascript",
-    icon: SiJavascript,
-  },
-  {
-    _id: 5,
-    name: "React JS",
-    icon: SiReact,
-  },
-  {
-    _id: 6,
-    name: "Nextjs",
-    icon: TbBrandNextjs,
-  },
-  {
-    _id: 7,
-    name: "React MUI",
-    icon: SiMui,
-  },
-  {
-    _id: 8,
-    name: "Tailwind",
-    icon: SiTailwindcss,
-  },
-  {
-    _id: 9,
-    name: "Node JS",
-    icon: FaNodeJs,
-  },
-  {
-    _id: 10,
-    name: "Express JS",
-    icon: SiExpress,
-  },
-  {
-    _id: 11,
-    name: "TypeScript",
-    icon: SiTypescript,
-  },
-  {
-    _id: 12,
-    name: "MongoDB",
-    icon: SiMongodb,
-  },
-  {
-    _id: 13,
-    name: "MySQL",
-    icon: SiMysql,
-  },
-  {
-    _id: 14,
-    name: "Firebase",
-    icon: SiFirebase,
-  },
-  {
-    _id: 15,
-    name: "Git / GitHub",
-    icon: SiGithub,
-  },
-  {
-    _id: 16,
-    name: "Python",
-    icon: DiPython,
-  },
-  {
-    _id: 17,
-    name: "C++",
-    icon: CgCPlusPlus,
-  },
-  {
-    _id: 18,
-    name: "Java",
-    icon: SiJava,
-  },
-];
+        _id: 18,
+        name: "Java",
+        icon: CgCPlusPlus,
+    },
+
+]
