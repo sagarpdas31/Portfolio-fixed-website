761101e765112b8429a79b5e7f53cc3296d8d85a
SAGAR KUMAR DAS <108587278+Sagarkumardas2002@users.noreply.github.com>
2026-04-12 18:48:47 +0530

    Update WorkExp.js

--- client/src/pages/WorkExp/WorkExp.js
+++ client/src/pages/WorkExp/WorkExp.js
@@ -16,23 +16,28 @@ const WorkExp = () => {
                     </h2>
                     <hr />
                     <VerticalTimeline lineColor="#1e1e2c">
-                        <VerticalTimelineElement
-                            className="vertical-timeline-element--work"
-                            contentStyle={{ background: "white", color: "#1e1e2c" }}
-                            contentArrowStyle={{
-                                borderRight: "7px solid  white",
-                            }}
-                            date="2023- Present"
-                            iconStyle={{ background: "#1e1e2c", color: "#fff" }}
-                            icon={<SiReact />}
-                        >
-                            <h3 className="vertical-timeline-element-title">
-                                MERN Stack Developer
-                            </h3>
-                            <p>
-                                Efficient, Responsive, Attractive , Performance centric web applications
-                            </p>
-                        </VerticalTimelineElement>
+                  <VerticalTimelineElement
+                        className="vertical-timeline-element--work"
+                        contentStyle={{ background: "white", color: "#1e1e2c" }}
+                        contentArrowStyle={{
+                            borderRight: "7px solid  white",
+                        }}
+                        date="April 2025 - July 2025"
+                        iconStyle={{ background: "#1e1e2c", color: "#fff" }}
+                        icon={<SiReact />}
+                    >
+                        <h3 className="vertical-timeline-element-title">
+                            Web Developer Intern 
+                        </h3>
+                        <h4>
+                            Zidio Development Ltd
+                        </h4>
+                        <ul>
+                            <li>Built reusable React.js components</li>
+                            <li>Improved performance by 30% (lazy loading, Redux optimization)</li>
+                            <li>Integrated REST APIs for scalable backend communication</li>
+                        </ul>
+                    </VerticalTimelineElement>
                         <VerticalTimelineElement
                             className="vertical-timeline-element--work"
                             contentStyle={{ background: "white", color: "#1e1e2c" }}
@@ -44,7 +49,7 @@ const WorkExp = () => {
                             icon={<SiReact />}
                         >
                             <h3 className="vertical-timeline-element-title">
-                                Full Stack Developer
+                               MERN Full Stack Developer
                             </h3>
                             <p>
                                 Creative Design, User Experience, Visual Design, Project
@@ -69,23 +74,7 @@ const WorkExp = () => {
                                 Array, String, List, Sorting & Searching Algorithms, Recursion, Dynamic Programming, Tree, Graph, Trie.
                             </p>
                         </VerticalTimelineElement>
-                        <VerticalTimelineElement
-                            className="vertical-timeline-element--work"
-                            contentStyle={{ background: "white", color: "#1e1e2c" }}
-                            contentArrowStyle={{
-                                borderRight: "7px solid  white",
-                            }}
-                            date="2024 May - Present"
-                            iconStyle={{ background: "#1e1e2c", color: "#fff" }}
-                            icon={<SiReact />}
-                        >
-                            <h3 className="vertical-timeline-element-title">
-                                Next.Js
-                            </h3>
-                            <p>
-                                New to Next.js, gaining knowledge in SSR, dynamic routing, API integration, and optimizing full-stack applications.
-                            </p>
-                        </VerticalTimelineElement>
+                      
 
                         <VerticalTimelineElement
                             className="vertical-timeline-element--work"
