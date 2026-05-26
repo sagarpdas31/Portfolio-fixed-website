255206ca9fa943cb6cd66b7325c39a27f975a394
SAGAR KUMAR DAS <mrsagarkumardas5g9327@gmail.com>
2026-05-26 18:40:53 +0530

    Update cron.js

--- server/cron/cron.js
+++ server/cron/cron.js
@@ -6,7 +6,7 @@ const job = new cron.CronJob("*/14 * * * *", function () {
     https
         .get(URL, (res) => {
             if (res.statusCode === 200) {
-                console.log("GET request sent successfully");
+                console.log("GET request sent successfully!");
             } else {
                 console.log("GET request failed", res.statusCode);
             }
